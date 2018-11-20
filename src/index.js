8836bff5fa1a8e55f91ec76780507b6b77c7e354
Kevin W <wongkokwah@gmail.com>
2018-11-20 11:03:02 +0800

    add private and public routes

--- src/index.js
+++ src/index.js
@@ -3,16 +3,22 @@ import ReactDOM from 'react-dom';
 import './resources/css/app.css';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
-import './firebase';
+import { firebase } from './firebase';
 
-const App = () => {
+//reveive user props from ReactDOM.render(<App user={user}/>
+const App = (props) => {
   return (
     <BrowserRouter>
-      <Routes />
+      <Routes {...props}/>
     </BrowserRouter>
 
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+firebase.auth().onAuthStateChanged((user) => {
+  // console.log(user);
+  ReactDOM.render(<App user={user}/>, document.getElementById('root'));
+})
+
+
 
--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,18 @@ import React from 'react';
 import Layout from './hoc/Layout';
 import {Switch, Route} from 'react-router-dom';
 import Home from './components/home/Home';
+import Login from './components/login/Login';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/authRoutes/PrivateRoute';
+import PublicRoute from './components/authRoutes/PublicRoute';
 
 const Routes = (props) => {
   return (
     <Layout>
       <Switch>
-        <Route exact component={Home} path="/" />
+        <PrivateRoute {...props} path="/dashboard" exact component={Dashboard}/>
+        <PublicRoute {...props} restricted={true}path="/login" exact component={Login} />
+        <PublicRoute {...props} restricted={false} path="/" exact component={Home} />
       </Switch>
     </Layout>
   );
