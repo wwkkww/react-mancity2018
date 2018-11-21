98eafb7526d0d07669f21b2c220b42401f577b68
Kevin W <wongkokwah@gmail.com>
2018-11-21 10:32:28 +0800

    add create new match  & edit matches

--- src/firebase.js
+++ src/firebase.js
@@ -2,20 +2,21 @@ import * as firebase from 'firebase';
 import 'firebase/app';
 import 'firebase/database';
 
-  const config = {
-    apiKey: "AIzaSyCiQUWNJN4yxQajDm9rQhtt8nXLOCffvIU",
-    authDomain: "mancity-21790.firebaseapp.com",
-    databaseURL: "https://mancity-21790.firebaseio.com",
-    projectId: "mancity-21790",
-    storageBucket: "mancity-21790.appspot.com",
-    messagingSenderId: "529285832774"
-  };
+const config = {
+  apiKey: "AIzaSyCiQUWNJN4yxQajDm9rQhtt8nXLOCffvIU",
+  authDomain: "mancity-21790.firebaseapp.com",
+  databaseURL: "https://mancity-21790.firebaseio.com",
+  projectId: "mancity-21790",
+  storageBucket: "mancity-21790.appspot.com",
+  messagingSenderId: "529285832774"
+};
 
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
 
-  const firebaseDB = firebase.database();
-  const matchesDB = firebaseDB.ref('matches');
-  const promotionsDB = firebaseDB.ref('promotions');
+const firebaseDB = firebase.database();
+const matchesDB = firebaseDB.ref('matches');
+const promotionsDB = firebaseDB.ref('promotions');
+const teamsDB = firebaseDB.ref('teams');
 
 
-  export { firebase, matchesDB, promotionsDB, firebaseDB as default };
+export { firebase, matchesDB, teamsDB, promotionsDB, firebaseDB as default };
--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,10 @@ import React from 'react';
 import Layout from './hoc/Layout';
 import {Switch, Route} from 'react-router-dom';
 import Home from './components/home/Home';
-import Login from './components/login/Login';
 import Dashboard from './components/dashboard/Dashboard';
+import Login from './components/login/Login';
+import Matches from './components/dashboard/matches/Matches';
+import AddEditMatch from './components/dashboard/matches/AddEditMatch';
 import PrivateRoute from './components/authRoutes/PrivateRoute';
 import PublicRoute from './components/authRoutes/PublicRoute';
 
@@ -11,6 +13,9 @@ const Routes = (props) => {
   return (
     <Layout>
       <Switch>
+        <PrivateRoute {...props} path="/matches" exact component={Matches}/>
+        <PrivateRoute {...props} path="/matches/editmatch" exact component={AddEditMatch}/>
+        <PrivateRoute {...props} path="/matches/editmatch/:id" exact component={AddEditMatch}/>
         <PrivateRoute {...props} path="/dashboard" exact component={Dashboard}/>
         <PublicRoute {...props} restricted={true}path="/login" exact component={Login} />
         <PublicRoute {...props} restricted={false} path="/" exact component={Home} />
