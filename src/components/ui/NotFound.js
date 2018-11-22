9c808ddf1b11323374f0df381c75a63c48229c70
Kevin W <wongkokwah@gmail.com>
2018-11-22 17:58:17 +0800

    add page 404. enable public user view the team page

---
+++ src/components/ui/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div className="not_found_container">
+            <h3>Sorry</h3>
+            <div><h4>Page not found</h4></div>
+        </div>
+    );
+};
+
+export default NotFound;
